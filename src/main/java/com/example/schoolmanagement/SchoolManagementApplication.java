3b1b5f0b2cd57a46b0d9205f73f8738f5561a25d
Lütfü Heysem <heysem1104@gmail.com>
2025-04-04 20:43:08 +0300

    dependencies and properties updated to use local excel file instead of database

--- src/main/java/com/example/schoolmanagement/SchoolManagementApplication.java
+++ src/main/java/com/example/schoolmanagement/SchoolManagementApplication.java
@@ -2,12 +2,25 @@ package com.example.schoolmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
+@Configuration
 @SpringBootApplication
 public class SchoolManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SchoolManagementApplication.class, args);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2) // Use an in-memory H2 database
+                .build();
     }
 
 }
